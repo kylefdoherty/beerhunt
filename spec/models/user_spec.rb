c890afcdde7d218aa869f37173f768cb6a2791e5
kylefdoherty <kyledoherty22@gmail.com>
2016-03-28 10:27:27 -0400

    Add test for welcome email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,23 @@
 require "rails_helper"
 
 describe User do
+  include ActiveJob::TestHelper
+
   describe ".from_omniauth" do
+    before { clear_enqueued_jobs }
+
     context "when the user is new" do
       it "creates a new user from the oauth info" do
         User.from_omniauth(auth_hash)
 
         expect(User.all.count).to eq(1)
         expect(User.last.name).to eq(auth_hash["info"]["name"])
+      end
+
+      it "sends a welcome email" do
+        User.from_omniauth(auth_hash)
+
+        expect(enqueued_jobs.size).to eq(1)
       end
     end
 
@@ -22,6 +32,17 @@ describe User do
 
         found_user = User.from_omniauth(auth_hash)
         expect(found_user.id).to eq(bruce.id)
+      end
+
+      it "does not send a welcome email" do
+        bruce = create(:user,
+                       name: "Bruce Wayne",
+                       email: "brucewayne@gmail.com",
+                       provider: "facebook",
+                       uid: "10255214431077214"
+                      )
+
+        expect(enqueued_jobs.size).to eq(0)
       end
     end
   end
