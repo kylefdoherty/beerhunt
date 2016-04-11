9a5fe02183c3dc84b63b506c83a74eaecaf7c142
kylefdoherty <kyledoherty22@gmail.com>
2016-04-11 18:33:23 -0400

    Add codeclimate.yml

--- spec/features/like_beer_spec.rb
+++ spec/features/like_beer_spec.rb
@@ -12,6 +12,17 @@ feature "like beer", js: true do
 
     expect(page).to have_css("span.like-count", text: 1)
   end
+
+  scenario "user who's logged in unlikes a beer" do
+    beer = create(:beer)
+    user_logs_in
+    # likes the beer
+    find(".like-btn").click
+    # unlikes the beer
+    find(".like-btn").click
+
+    expect(page).to have_css("span.like-count", text: 0)
+  end
 end
 
 def user_logs_in
